530e9e45df4373fe5f08c9b877d0bd2bf4f5ca87
Wojciech Kalebasiak <kalebasiakw@gmail.com>
2018-04-23 20:31:25 +0200

    Add promise

--- components/App.js
+++ components/App.js
@@ -6,48 +6,51 @@ App = React.createClass({
             gif: {}
         };
     },
-    getGif: function(searchingText, callback) {
-      var GIPHY_API_KEY = 'ZR9hGrtAmWg0L6YHSjneQ3kqsnV51Dbc';
-      var GIPHY_API_URL = 'http://api.giphy.com';
-        var url = GIPHY_API_URL + '/v1/gifs/random?api_key='+ GIPHY_API_KEY + '&tag=' + searchingText;
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', url);
-        xhr.onload = function() {
-            if (xhr.status === 200) {
-                var data = JSON.parse(xhr.responseText).data;
-                var gif = {
-                    url: data.fixed_width_downsampled_url,
-                    sourceUrl: data.url
-                };
-                callback(gif);
+    getGif: function(searchingText) {
+        const GIPHY_API_KEY = 'ZR9hGrtAmWg0L6YHSjneQ3kqsnV51Dbc';
+        const GIPHY_API_URL = 'http://api.giphy.com';
+        const url = GIPHY_API_URL + `/v1/gifs/random?api_key=${GIPHY_API_KEY}&tag=${searchingText}`;
+        return new Promise((resolve, reject) => {
+            const xhr = new XMLHttpRequest();
+            xhr.open('GET', url);
+            xhr.onload = function() {
+                if (xhr.status === 200) {
+                    const data = JSON.parse(this.responseText).data;
+                    const gif = {
+                        url: data.fixed_width_downsampled_url,
+                        sourceUrl: data.url
+                    }
+                    resolve(gif);
+                } else {
+                    reject(new Error(`Error ${this.statusText}`));
+                }
             }
-        };
-        xhr.send();
+            xhr.onerror = function() {
+                reject(new Error(`Eror ${this.statusText}`));
+            }
+            xhr.send();
+        });
     },
     handleSearch: function(searchingText) {
         this.setState({
             loading: true
         });
-        this.getGif(searchingText, function(gif) {
+        this.getGif(searchingText).then(gif => {
             this.setState({
                 loading: false,
                 gif: gif,
                 searchingText: searchingText
-            })
-        }.bind(this));
+            });
+        }).catch(err => console.log(err));
     },
-    render: function() {
-        return (
-          <div className = "app">
-            <h1> Gif Searcher</h1>
-            <p> Find gif on <a href = "http://giphy.com">giphy.</a> Press enter to get more gifs!</p>
+render: function() {
+    return (
+        <div className = "app" >
+            <h1>Gif Searcher </h1>
+            <p>Find gif on <a href = "http://giphy.com">giphy.</a> Press enter to get more gifs!</p>
             <Search onSearch = { this.handleSearch }/>
-            <Gif
-              loading = { this.state.loading }
-              url = { this.state.gif.url }
-              sourceUrl = { this.state.gif.sourceUrl }
-            />
-            </div>
-        );
-    }
+            <Gif loading = { this.state.loading } url = { this.state.gif.url } sourceUrl = { this.state.gif.sourceUrl }/>
+        </div>
+    );
+}
 });
